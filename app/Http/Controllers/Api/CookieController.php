a7b5d1922df8b3e50ac32f0a83f794f452015141
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-02-23 15:23:14 +0100

    [add]:controller for api

--- app/Http/Controllers/Api/CookieController.php
+++ app/Http/Controllers/Api/CookieController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Http\Request;
 
 
 
@@ -18,5 +20,35 @@ class CookieController extends Controller
 
         ];
         return response()->json(compact('ads'));
+    }
+
+    public function saveSession(Request $request)
+    {
+        $valore = $request->input('valore');
+        // Salva il valore in sessione
+
+        Session::put('modal_disattivato', $valore);
+        $disable_modal = Session::all();
+
+
+        return response()->json(compact('disable_modal'));
+    }
+
+
+    public function session(Request $request)
+    {
+
+
+        // Ottieni il valore dalla sessione
+        $valore = Session::all();
+
+
+        // Esegui le azioni necessarie basate sul valore
+        if ($valore) {
+            return response()->json(compact('valore'));
+        } else {
+            return response()->json(compact('valore'));
+        }
+        // Restituisci una risposta o fai altro...
     }
 }
